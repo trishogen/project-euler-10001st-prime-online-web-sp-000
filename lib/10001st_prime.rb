119e30186d1394f9ba38e7f842ef0677a24c0653
trishogen <patriciaogen@gmail.com>
2020-10-03 11:20:09 -0400

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,30 @@
-# Implement your procedural solution here!
+def prime_number_for(num)
+  current_num = 2
+  count = 1
+
+  while count < num
+    current_num += 1
+
+    if is_prime?(current_num)
+      count += 1
+    end
+
+  end
+
+  return current_num
+end
+
+
+def is_prime?(num)
+  current = 2
+
+  while current <= Math.sqrt(num).round
+    if num % current == 0
+      return false
+    end
+
+    current += 1
+  end
+
+  return true
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,36 @@
-# Implement your object-oriented solution here!
+class Prime
+
+  def initialize(num)
+    @num = num
+  end
+
+  def number
+    current_num = 2
+    count = 1
+
+    while count < @num
+      current_num += 1
+
+      if is_prime?(current_num)
+        count += 1
+      end
+
+    end
+
+    return current_num
+  end
+
+  def is_prime?(num)
+    current = 2
+
+    while current <= Math.sqrt(num).round
+      if num % current == 0
+        return false
+      end
+
+      current += 1
+    end
+
+    return true
+  end
+end
